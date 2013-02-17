3787070648db1f15662c6ad2e583ee03f8d4d664
Adron Hall <adron@basho.com>
2013-02-16 21:41:16 -0800

    Added mocha and the first test with a test directory in the test.js file.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require("assert")
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should verify the world is how we left it, with 2+2 resulting in 4.', function(){
+    	var result = 2 + 2;
+      assert.equal(result, 4);
+    })
+  })
+})
