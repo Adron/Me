01b60ca1ff1585f16a7f97b4839f2f01fb321694
Adron Hall <adron@basho.com>
2013-03-17 16:43:58 -0700

    Added a comment to file.

--- test/figure_out_riak_client.js
+++ test/figure_out_riak_client.js
@@ -2,3 +2,4 @@ var baseUrl = "http://localhost:8098/riak";
 var mapredUrl = "http://localhost:8098/mapred";
 var client = new RiakClient(baseUrl, mapredUrl);
 
+// obviously not done, don't hurt yourself trying to figure out what these bits are for.  ;)
