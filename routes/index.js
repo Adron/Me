ebcbb2d4d1ffe784ecd4eac3c68166c1db955ce5
Adron Hall <adron@basho.com>
2013-03-17 16:44:54 -0700

    Added temp code in place for retrieval of data.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,33 @@
-exports.index = function(req, res){
-    res.render('index', { title: 'Vcard @adron' });
-};
+var GitHubApi = require("github");
 
 exports.github = function(req, res){
-	res.render('github', { title: 'Github' });
+	var github = new GitHubApi({
+	    // required
+	    version: "3.0.0",
+	    // optional
+	    timeout: 5000,
+	});
+
+	var githubBits;
+	var githubOrgName;
+
+	github.authenticate({
+	    type: "basic",
+	    username: "adron",
+	    password: "jumkpassword"
+	});
+
+	github.orgs.get({
+	  org: "Basho"
+	}, function(err, githubBits){
+	  console.log(githubBits);
+	  githubOrgName = githubBits;
+	});
+
+
+	res.render('github', { title: 'Github', githubbinBasho: githubOrgName });
+};
+
+exports.index = function(req, res){
+    res.render('index', { title: 'Vcard @adron' });
 };
