a55b4b3823dcbac68315bc8e34bc63509dadce65
Adron Hall <adron@basho.com>
2013-03-17 11:15:55 -0700

    added app.js routing for routing.

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/github', routes.github);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
