a653b3fa8fbbb6dcef98bcdd91b36352e705d1f3
Adron Hall <adron@basho.com>
2013-03-17 23:02:36 -0700

    Added a few more tabs and such for future reference points of the direction Im going with the site.

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,7 @@
 var GitHubApi = require("github");
 
 exports.github = function(req, res){
-	var github = new GitHubApi({
-	    // required
-	    version: "3.0.0",
-	    // optional
-	    timeout: 5000,
-	});
-
-	var githubBits;
-	var githubOrgName;
-
-	github.authenticate({
-	    type: "basic",
-	    username: "adron",
-	    password: "jumkpassword"
-	});
-
-	github.orgs.get({
-	  org: "Basho"
-	}, function(err, githubBits){
-	  console.log(githubBits);
-	  githubOrgName = githubBits;
-	});
-
-
-	res.render('github', { title: 'Github', githubbinBasho: githubOrgName });
+	res.render('github', { title: 'Github' });
 };
 
 exports.index = function(req, res){
