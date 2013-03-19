871fba0f9517c162e724c5fcb8fb9dca35669ee3
Adron Hall <adron@basho.com>
2013-03-19 14:03:12 -0700

    Putting them back.

---
+++ test/figure_out_riak_client.js
@@ -0,0 +1,5 @@
+var baseUrl = "http://localhost:8098/riak";
+var mapredUrl = "http://localhost:8098/mapred";
+var client = new RiakClient(baseUrl, mapredUrl);
+
+// obviously not done, don't hurt yourself trying to figure out what these bits are for.  ;)
