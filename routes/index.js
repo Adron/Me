736a19634f9e24a64f4f84a5a192fa9f2e555b56
Adron Hall <adronhall@gmail.com>
2013-09-02 00:50:06 -0700

    committing route.

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
 exports.index = function(req, res){
     res.render('index', { title: 'Vcard @adron' });
 };
+
+exports.github = function(req, res){
+	res.render('github', { title: 'Github Activity' });
+}
